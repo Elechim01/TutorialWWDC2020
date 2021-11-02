6d41a48446c3112b6ac34ca4f5298c1af81f757f
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-11-02 23:51:21 +0100

    implementato lettore musicale con lo slider circolare

---
+++ TutorialWWDC2020/CustomMusicPlayerCircularSlider/Model/AlbumCustomMiusicPlayer.swift
@@ -0,0 +1,15 @@
+//
+//  AlbumCustomMiusicPlayr.swift
+//  TutorialWWDC2020
+//
+//  Created by Michele Manniello on 02/11/21.
+//
+
+import SwiftUI
+
+struct AlbumCustomMiusicPlayer {
+    var title: String = ""
+    var artist: String = ""
+    var type: String = ""
+    var artwork: UIImage = UIImage(named: "g1")!
+}
---
+++ TutorialWWDC2020/CustomMusicPlayerCircularSlider/ViewCustomMusicPlayerCircularSlider.swift
@@ -0,0 +1,20 @@
+//
+//  ViewCustomMusicPlayerCircularSlider.swift
+//  TutorialWWDC2020
+//
+//  Created by Michele Manniello on 02/11/21.
+//
+
+import SwiftUI
+
+struct ViewCustomMusicPlayerCircularSlider: View {
+    var body: some View {
+       HomeViewCustomMusicPlayerCircularSlider()
+    }
+}
+
+struct ViewCustomMusicPlayerCircularSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        ViewCustomMusicPlayerCircularSlider()
+    }
+}
